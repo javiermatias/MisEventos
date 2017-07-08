ae3292527cca4c2c723292366856afdaea53bd2b
augustogalan7 <augustogalan7@gmail.com>
2017-07-08 17:55:39 -0300

    ABM Espacio

--- Renacer.Web/src/app/pages/espacios/espacios.component.ts
+++ Renacer.Web/src/app/pages/espacios/espacios.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input,ChangeDetectorRef} from '@angular/core';
 import {EspacioComun,EspacioServices} from '../../resources/espacio.service';
 import {FormGroup} from '@angular/forms';
 import {DatePipe} from '@angular/common' ;
@@ -11,41 +11,60 @@ import {DatePipe} from '@angular/common' ;
 export class EspaciosComponent implements OnInit {
 
   public _espacio = new EspacioComun(0,"","",0,0,10);
-  public espacios = new Array<EspacioComun>();
+  @Input() espacios = new Array<EspacioComun>();
   public showDetail:boolean = false;
-  constructor(private _espacioService:EspacioServices) { }
+
+  constructor(private _espacioService:EspacioServices,private ref: ChangeDetectorRef) {
+
+  }
 
   ngOnInit() {
-    this.espacios =  this._espacioService.query()
-    let espacioEjemplo = new EspacioComun(0,"Mi casa","prueba",0,0,10);
-    this.espacios.push(espacioEjemplo);
+    this.getItems();
   }
 
-  onSubmit(myForm: FormGroup) {
-    let newEspacio = Object.assign({}, this._espacio);
-    this._espacio = new EspacioComun(0,"","",0,0,10);
-    newEspacio.fechaCreacion = new Date();
-    this.saveEspacio(newEspacio)
-    this.espacios.push(newEspacio)
-    myForm.reset();
-  }
-
-  verItem(item:EspacioComun){
-    this._espacio = item;
-    this.showDetail = true;
-  }
-
-  saveEspacio(item:EspacioComun):any{
-      if(item.id == 0){
-        this._espacioService.save(item,function(response) {
-          return response;
-        })
-      }else{
-        this._espacioService.update(item)
-        return "ok";
-      }
-
-      console.log("submit Espacio");
+  getItems(){
+    this._espacioService.query({},(items:EspacioComun[]) => {
+      this.espacios = items;
     }
+  );
+}
+
+onSubmit(myForm: FormGroup) {
+  let newEspacio = Object.assign({}, this._espacio);
+  this._espacio = new EspacioComun(0,"","",0,0,10);
+  // newEspacio.fechaCreacion = new Date();
+  this.saveItem(newEspacio)
+  myForm.reset();
+}
+
+verItem(item:EspacioComun){
+  this._espacio = Object.assign({}, item);
+  this.showDetail = true;
+}
+nuevoItem(){
+  this._espacio =  new EspacioComun(0,"","",0,0,10);
+  this.showDetail = true;
+}
+
+saveItem(item:EspacioComun):any{
+  if(item.id == 0){
+    this._espacioService.save(item,(resp:EspacioComun) => {
+      item = resp;
+      this.espacios.push(item);
+      this.showDetail = false;
+    });
+  }else{
+    this._espacioService.update(item,(resp:EspacioComun) => {
+      let espacios = this.espacios;
+      for (var i = 0; i < espacios.length; i++) {
+        if(espacios[i].id == resp.id)
+        { espacios[i] = resp;}
+         this.ref.detectChanges();
+      }
+      this.showDetail = false;
+    });
+  }
+}
+
 
 }
