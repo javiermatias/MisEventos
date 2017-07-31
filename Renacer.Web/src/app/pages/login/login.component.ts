682aea7b0c9900c698e8a97992dc45601a2b1e69
Marcos Donalisio <marcosdonalisio@gmail.com>
2017-07-31 00:20:53 -0300

    Se eliminaron los comentarios en el Seed

--- Renacer.Web/src/app/pages/login/login.component.ts
+++ Renacer.Web/src/app/pages/login/login.component.ts
@@ -12,54 +12,54 @@ import {Sha256} from "sha256";
 })
 export class LoginComponent {
   public router: Router;
-  public form:FormGroup;
-  public username:AbstractControl;
-  public password:AbstractControl;
-  public error :string;
-  public loading:boolean = false;
+  public form: FormGroup;
+  public username: AbstractControl;
+  public password: AbstractControl;
+  public error: string;
+  public loading: boolean = false;
 
-  constructor(router:Router, fb:FormBuilder,private _usersService:UserServices) {
-  // constructor(router:Router, fb:FormBuilder,private _usersService:UserServices) {
+  constructor(router: Router, fb: FormBuilder, private _usersService: UserServices) {
+    // constructor(router:Router, fb:FormBuilder,private _usersService:UserServices) {
     this.router = router;
     this.form = fb.group({
-      'username': ['', Validators.compose([Validators.required, Validators.required])],
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+      'username': ["", Validators.compose([Validators.required, Validators.required])],
+      'password': ["", Validators.compose([Validators.required, Validators.minLength(6)])]
     });
 
     this.username = this.form.controls['username'];
-    this.password =this.form.controls['password'];
+    this.password = this.form.controls['password'];
   }
 
-  public onSubmit(values:Object):void {
+  public onSubmit(values: Object): void {
     if (this.form.valid) {
-    //  let credenciales = {
-    //    "usuario":this.username.value,
-    //    "clave": Sha256(this.password.value)
-    //  }
-     let credenciales = {
-       "usuario":this.username.value,
-       "clave": this.password.value
-     }
-       this.loading = true;
-       this._usersService.login(credenciales,(result)=>{
-         this.loading = false;
+      //  let credenciales = {
+      //    "usuario":this.username.value,
+      //    "clave": Sha256(this.password.value)
+      //  }
+      let credenciales = {
+        "usuario": this.username.value,
+        "clave": this.password.value
+      }
+      this.loading = true;
+      this._usersService.login(credenciales, (result) => {
+        this.loading = false;
 
-        if(result["result"] == "ok"){
+        if (result["result"] == "ok") {
 
           this._usersService.setCurrent(result["user"]);
 
           this.router.navigate(['pages/dashboard']);
-        }else{
-          this.error = "Error en el usuario o contraseña"
+        } else {
+          this.error = "Error en el usuario o contraseña";
         }
       });
     }
   }
 }
 
-export function emailValidator(control: FormControl): {[key: string]: any} {
+export function emailValidator(control: FormControl): { [key: string]: any } {
   var emailRegexp = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
   if (control.value && !emailRegexp.test(control.value)) {
-    return {invalidEmail: true};
+    return { invalidEmail: true };
   }
 }
