ac59a03a96028467826fc87d5b6c971feed719b6
lucasgonzalezm <lucasgonzalezm3@gmail.com>
2019-06-05 19:29:30 -0300

    docs(general): se corrigen varios tildes en matricula y recordatorio

--- Renacer.Web/src/app/pages/recordatorios/recordatorios.component.ts
+++ Renacer.Web/src/app/pages/recordatorios/recordatorios.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
-import {  ToastrService } from 'ngx-toastr';
+import { ToastrService } from 'ngx-toastr';
 import { RecordatorioServices } from '../../resources/recordatorio.service';
 import { Recordatorio } from '../../models/recordatorio';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
@@ -12,66 +12,66 @@ import { analyzeAndValidateNgModules } from '@angular/compiler';
 export class RecordatoriosComponent implements OnInit {
   //items=[];
   showRecordatorio: boolean = false;
-  borrar:boolean = true;
- 
+  borrar: boolean = true;
+
   recordatorios: Recordatorio[];
-  
-  constructor(private _recordatorioService: RecordatorioServices,private toastrService: ToastrService) { }
+
+  constructor(private _recordatorioService: RecordatorioServices, private toastrService: ToastrService) { }
 
   ngOnInit() {
-  
+
     this.getItems();
- 
+
   }
 
   getItems() {
-   
+
     this._recordatorioService.query({}, (items: Recordatorio[]) => {
-      console.log(items);
+      // console.log(items);
       this.recordatorios = items;
-      console.log(this.recordatorios);
-      
+      // console.log(this.recordatorios);
+
     });
   }
 
-  cerrar(reco:Recordatorio){
+  cerrar(reco: Recordatorio) {
     //console.log("Hizo Click en borrar");
     //aca tengo que hacer el cuadro de dialogo
-    let id:any;
-    id=reco.id;
-    console.log(reco.id);
-    if(confirm("Estas seguro de eliminar el recordatorio " + reco.descripcion)) {
-      console.log("pase por el si");
-      this._recordatorioService.remove({id},(resp:any) => {
+    let id: any;
+    id = reco.id;
+    // console.log(reco.id);
+    if (confirm("¿Estás seguro de eliminar el recordatorio " + reco.descripcion)) {
+      // console.log("pase por el si");
+      this._recordatorioService.remove({ id }, (resp: any) => {
         //Callback
-           this.getItems();
-          this.toastrService.success('Se elimino el recordatorio!', 'Aviso!');
-          //this.volver();
-     });
+        this.getItems();
+        this.toastrService.success('¡Se eliminó el recordatorio!', 'Aviso!');
+        //this.volver();
+      });
     }
 
-  
+
 
 
     //this.toastrService.warning('Esta seguro de eliminar el recordatorio?');
   }
 
-  nuevoRecordatorio(){
+  nuevoRecordatorio() {
     this.showRecordatorio = true;
   }
 
-  guardo(cancelar: boolean){
+  guardo(cancelar: boolean) {
     //console.log("pase por guarod");
-    console.log(cancelar);
-    if(cancelar){
+    // console.log(cancelar);
+    if (cancelar) {
       this.showRecordatorio = false;
       //console.log("se hizo click en cancelar");
-    }else{
+    } else {
       this.showRecordatorio = false;
       this.getItems();
       //console.log("se hizo click en guardar");
     }
-    
+
   }
 
 }
