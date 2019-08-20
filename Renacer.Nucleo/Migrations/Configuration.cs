a192222aa7a13933f23764e726af86647ea1f9a3
augustogalan7 <augustogalan7@gmail.com>
2019-08-20 19:11:28 -0300

    agregar comprobante de pago cuota

--- Renacer.Nucleo/Migrations/Configuration.cs
+++ Renacer.Nucleo/Migrations/Configuration.cs
@@ -97,34 +97,34 @@ namespace Renacer.Nucleo.Migrations
             context.matriculaXSocio.AddOrUpdate(matriculaxsocio);
 
             //Carga de roles
-            Rol admin = new Rol() { nombre = "Administrador", descripcion = "Administrador del sistema" };
-            Rol secre = new Rol() { nombre = "Secretario", descripcion = "Encargado de Recepción" };
-            Rol encargado = new Rol() { nombre = "Encargado", descripcion = "Encargado de evento" };
-            Rol socioRol = new Rol() { nombre = "Socio", descripcion = "Interesado en asistir a un evento" };
+            //Rol admin = new Rol() { nombre = "Administrador", descripcion = "Administrador del sistema" };
+            //Rol secre = new Rol() { nombre = "Secretario", descripcion = "Encargado de Recepción" };
+            //Rol encargado = new Rol() { nombre = "Encargado", descripcion = "Encargado de evento" };
+            //Rol socioRol = new Rol() { nombre = "Socio", descripcion = "Interesado en asistir a un evento" };
 
             //TipoDocumento _tipoDoc = new TipoDocumento() { nombre = "dni" };
 
-            Persona _personaAdmin = new Persona() { nombre = "Javier", apellido = "Jimenez", tipoDoc = tipoDoc, nroDocumento = "32" };
-            Persona _personaSecretaria = new Persona() { nombre = "Luciana", apellido = "Quinteros", tipoDoc = tipoDoc, nroDocumento = "33" };
-            Persona _personaEncargado = new Persona() { nombre = "Lucas", apellido = "Gonzalez", tipoDoc = tipoDoc, nroDocumento = "34" };
-            Persona _personaSocia = new Persona() { nombre = "Augusto", apellido = "Galán", tipoDoc = tipoDoc, nroDocumento = "35" };
+            //Persona _personaAdmin = new Persona() { nombre = "Javier", apellido = "Jimenez", tipoDoc = tipoDoc, nroDocumento = "32" };
+            //Persona _personaSecretaria = new Persona() { nombre = "Luciana", apellido = "Quinteros", tipoDoc = tipoDoc, nroDocumento = "33" };
+            //Persona _personaEncargado = new Persona() { nombre = "Lucas", apellido = "Gonzalez", tipoDoc = tipoDoc, nroDocumento = "34" };
+            //Persona _personaSocia = new Persona() { nombre = "Augusto", apellido = "Galán", tipoDoc = tipoDoc, nroDocumento = "35" };
 
-            List<Rol> rolesAdmin = new List<Rol>();
-            rolesAdmin.Add(admin);
-            rolesAdmin.Add(secre);
-            rolesAdmin.Add(encargado);
-            List<Rol> rolesSecre = new List<Rol>();
-            rolesSecre.Add(secre);
-            rolesSecre.Add(socioRol);
-            List<Rol> rolesEncar = new List<Rol>();
-            rolesSecre.Add(encargado);
-            List<Rol> rolesSocio = new List<Rol>();
-            rolesSocio.Add(socioRol);
+            //List<Rol> rolesAdmin = new List<Rol>();
+            //rolesAdmin.Add(admin);
+            //rolesAdmin.Add(secre);
+            //rolesAdmin.Add(encargado);
+            //List<Rol> rolesSecre = new List<Rol>();
+            //rolesSecre.Add(secre);
+            //rolesSecre.Add(socioRol);
+            //List<Rol> rolesEncar = new List<Rol>();
+            //rolesSecre.Add(encargado);
+            //List<Rol> rolesSocio = new List<Rol>();
+            //rolesSocio.Add(socioRol);
 
-            context.rol.AddOrUpdate(admin);
-            context.rol.AddOrUpdate(secre);
-            context.rol.AddOrUpdate(encargado);
-            context.rol.AddOrUpdate(socioRol);
+            //context.rol.AddOrUpdate(admin);
+            //context.rol.AddOrUpdate(secre);
+            //context.rol.AddOrUpdate(encargado);
+            //context.rol.AddOrUpdate(socioRol);
 
             //// Carga de usuarios
             context.usuario.AddOrUpdate(new Usuario() { nombre = "Administrador", usuario = "admin", rol = "Administrador", imagen = "assets/img/profile/users/augusto.png", clave = "123456", email = "admin@admin.com"}); // 123456 codificado queda: 8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92
