a0e64df0179a95157019359dc29d478942503f6f
augustogalan7 <augustogalan7@gmail.com>
2019-10-14 13:07:30 -0300

    +init back reporte ingresos

--- Renacer.Web/src/app/mis-eventos/reportes/ingresos-monetarios/ingresos-monetarios.component.ts
+++ Renacer.Web/src/app/mis-eventos/reportes/ingresos-monetarios/ingresos-monetarios.component.ts
@@ -20,8 +20,13 @@ export class IngresosMonetariosComponent implements OnInit {
     public lineChartOptions: any;
 
     public pieChartType = 'pie';
-    public pieChartLabels: Array<string>;
-    public pieChartData: Array<number>;
+
+    public IngresosTipoLabels: Array<string>;
+    public IngresosTipoData: Array<number>;
+
+    public IngresosTipoEventoLabels: Array<string>;
+    public IngresosTipoEventoData: Array<number>;
+
     public pieChartColors: any[];
     public ingresosPorTipoEventoOpt: any;
     public loading = true;
@@ -48,43 +53,72 @@ export class IngresosMonetariosComponent implements OnInit {
             {data: [], label: 'Cuotas' }
         ];
 
-        this.pieChartLabels = [];
-        this.pieChartData = [0, 0, 0, 0, 0];
 
+        let ingresos_en_meses = [
+            {fecha: '2019-01', ingresoMatricula: 100, ingresoCuota: 120}
+            , {fecha: '2019-02', ingresoMatricula: 30, ingresoCuota: 100}
+            , {fecha: '2019-03', ingresoMatricula: 60, ingresoCuota: 90}
+            ];
         // this._reporteServ.getCrecimientoSocios({}).subscribe(result => {
+            // ingresos_en_meses = result;
+            for (let i = 0; i < ingresos_en_meses.length; i++) {
+                    this.lineChartLabels.push(ingresos_en_meses[i].fecha)
+                    const  ingresosTotales = ingresos_en_meses[i].ingresoMatricula + ingresos_en_meses[i].ingresoCuota;
+                    this.lineChartData[0].data.push(ingresosTotales);
+                    this.lineChartData[1].data.push(ingresos_en_meses[i].ingresoMatricula);
+                    this.lineChartData[2].data.push(ingresos_en_meses[i].ingresoCuota);
+                }
+            // });
 
-        const ingresos_en_meses = [
-          {fecha: '2019-01', ingresoMatricula: 100, ingresoCuota: 120}
-          , {fecha: '2019-02', ingresoMatricula: 30, ingresoCuota: 100}
-          , {fecha: '2019-03', ingresoMatricula: 60, ingresoCuota: 90}
-        ];
 
-          for (let i = 0; i < ingresos_en_meses.length; i++) {
-                  this.lineChartLabels.push(ingresos_en_meses[i].fecha)
-                  const  ingresosTotales = ingresos_en_meses[i].ingresoMatricula + ingresos_en_meses[i].ingresoCuota;
-                  this.lineChartData[0].data.push(ingresosTotales);
-                  this.lineChartData[1].data.push(ingresos_en_meses[i].ingresoMatricula);
-                  this.lineChartData[2].data.push(ingresos_en_meses[i].ingresoCuota);
-              }
-        // })
-
-        // this._reporteServ.getIngresosTipoEvento({}).subscribe(result => {
-          const result_getIngresosTipoEvento = [
-            {cantidad: 3, nombre: 'Prueba 1'}
-            , {cantidad: 6, nombre: 'Prueba 2'}
-            , {cantidad: 8, nombre: 'Prueba 3'}
-          ];
-          this.ingresosPorTipoEventoOpt = this.config.pieChartOptions
-
-            this.pieChartData = [];
-            this.pieChartLabels = [];
+        let result_getIngresosTipoEvento = [
+            {cantidad: 3, nombre: 'Curso'}
+            , {cantidad: 6, nombre: 'Taller'}
+            , {cantidad: 8, nombre: 'Conferencia'}
+            ];
+            this.IngresosTipoEventoData = [];
+            this.IngresosTipoEventoLabels = [];
             result_getIngresosTipoEvento.forEach(item => {
-                this.pieChartData.push(item.cantidad);
-                this.pieChartLabels.push(item.nombre);
-              });
+                this.IngresosTipoEventoData.push(item.cantidad);
+                this.IngresosTipoEventoLabels.push(item.nombre);
+                });
+
+        this._reporteServ.getIngresosPorTipoEvento().subscribe(result => {
+            result_getIngresosTipoEvento = result;
+            this.ingresosPorTipoEventoOpt = this.config.pieChartOptions
+
+            this.IngresosTipoEventoData = [];
+            this.IngresosTipoEventoLabels = [];
+            result_getIngresosTipoEvento.forEach(item => {
+                this.IngresosTipoEventoData.push(item.cantidad);
+                this.IngresosTipoEventoLabels.push(item.nombre);
+                });
+        });
 
 
-        // })
+
+        this.IngresosTipoData = [];
+        this.IngresosTipoLabels = [];
+        const result_getIngresosTipo = [
+              {cantidad: 13, nombre: 'Eventos'}
+            , {cantidad: 8, nombre: 'Matrículas'}
+            ];
+
+        result_getIngresosTipo.forEach(item => {
+            this.IngresosTipoData.push(item.cantidad);
+            this.IngresosTipoLabels.push(item.nombre);
+        });
+
+        this._reporteServ.getIngresosPorTipo().subscribe(result => {
+            this.ingresosPorTipoEventoOpt = this.config.pieChartOptions
+            this.IngresosTipoData = [];
+            this.IngresosTipoLabels = [];
+            result.forEach(item => {
+                this.IngresosTipoData.push(item.cantidad);
+                this.IngresosTipoLabels.push(item.nombre);
+                });
+        });
+
     }
 
     public randomizeType(): void {
